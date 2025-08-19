47095fce8af627a58c36ce294ff996ee13ace0f1
hsuncho <hsuncho@naver.com>
2025-08-19 16:54:43 +0900

    #16 java: 정규표현식

---
+++ src/RegExpApp.java
@@ -0,0 +1,28 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExpApp {
+    public static void main(String[] args) {
+        /*
+        정규표현식
+        - 패턴을 정의하고 해당 패턴에 맞는 문자열 추출하거나, 치환과 같은 작업을 수행
+        - java.util.regex(Pattern, Matcher)
+        */
+
+        String txt = "저는 여자이고 제 인스타그램 계정은 hsuncho23@mail.com 이며 전화번호는 010-1234-1234";
+        Pattern pattern =  Pattern.compile("\\d{3}-\\d{4}-\\d{4}");
+        Matcher match = pattern.matcher(txt);
+        if(match.find()) {
+            System.out.println(">>> 전화번호 추출");
+            System.out.println(match.group());
+        }
+
+        // 이메일 패턴
+        String regex = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-z]{2,}";
+        match = Pattern.compile(regex).matcher(txt);
+        if(match.find()) {
+            System.out.println(">>> 이메일 추출");
+            System.out.println(match.group());
+        }
+    }
+}
